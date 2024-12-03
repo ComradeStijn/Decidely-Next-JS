8226c91df2124268ca49a61008be0cf088a7113d
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-12-03 22:53:24 +0100

    feat: Vote validation

--- app/components/VoteForms.tsx
+++ app/components/VoteForms.tsx
@@ -3,13 +3,25 @@
 import { Form } from "../booth/page";
 import { voteOnForm } from "./actions";
 import { useActionState } from "react";
-import { State } from "./actions"
+import { State } from "./actions";
+import { MoonLoader } from "react-spinners";
 
 export default function VoteForms({ form }: { form: Form }) {
-  const [state, formAction, isPending] = useActionState(voteOnForm, {errors: {}} as State)
+  const [state, formAction, isPending] = useActionState(voteOnForm, {
+    message: "",
+    errors: {},
+  } as State);
+
+  if (isPending) {
+    return (
+      <article className="flex min-h-44 items-center justify-center p-4 shadow-md">
+        <MoonLoader />
+      </article>
+    );
+  }
 
   return (
-    <form action={formAction} className="shadow-md p-4">
+    <form action={formAction} className="p-4 shadow-md">
       <h2 className="text-xl font-semibold">{form.title}</h2>
       <input type="hidden" name="formId" value={form.id} />
       {form.decisions.map((decision) => (
@@ -20,5 +32,5 @@ export default function VoteForms({ form }: { form: Form }) {
       ))}
       <button>Vote</button>
     </form>
-  )
+  );
 }
--- app/components/actions.ts
+++ app/components/actions.ts
@@ -1,39 +1,69 @@
-"use server"
-import { cookies } from "next/headers"
-import z from "zod"
-import jwt from "jsonwebtoken"
-import { redirect } from "next/navigation"
-import { DecodedToken } from "../booth/page"
+"use server";
+import { cookies } from "next/headers";
+import z from "zod";
+import jwt from "jsonwebtoken";
+import { redirect } from "next/navigation";
+import { DecodedToken } from "../booth/page";
+
+export type State = {
+  message?: string,
+  errors?: {
+    amount?: string;
+  };
+};
+
+type ProxyResponseBody = {
+  success: boolean;
+  message: number | string;
+};
 
 export async function voteOnForm(prevState: State, formData: FormData) {
-  const formId = formData.get("formId")
-  const formattedData: Record<string, string> = {}
+  const formId = formData.get("formId");
+  const formattedData: Record<string, string> = {};
 
-  const authToken = (await cookies()).get("auth_token")
-  if (!authToken || !authToken.value) redirect('/');
-  const decodedToken = jwt.decode(authToken.value) as unknown as DecodedToken
+  const authToken = (await cookies()).get("auth_token");
 
-  
+  let parsedProxyFetch: ProxyResponseBody;
+  try {
+    const proxyFetch = await fetch(
+      "https://decidely-api.onrender.com/forms/proxy",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken?.value}`,
+        },
+      },
+    );
+    parsedProxyFetch = await proxyFetch.json();
+  } catch (e) {
+    throw new Error(`Network error: ${e}`);
+  }
+
   for (const [key, value] of formData.entries()) {
-    if (!key.startsWith("$") && key !== 'formId') {
-      formattedData[key] = String(value)
+    if (!key.startsWith("$") && key !== "formId") {
+      formattedData[key] = isNaN(Number(value)) ? "0" : String(value);
     }
   }
 
-  const parsedData = DecisionSchema.safeParse(formattedData)
+  const parsedData = DecisionSchema.safeParse(formattedData);
   if (!parsedData.success) {
-    throw new Error("Critical Failure to Parse Data in Server Action")
+    throw new Error("Critical Failure to Parse Data in Server Action");
   }
 
-  console.log(parsedData)
-
-  return {}
-}
-
-const DecisionSchema = z.record(z.string().transform((val) => Number(val)))
-
-export type State = {
-  errors?: {
-    amount?: string
+  const castedVoteAmount = Object.values(parsedData.data).reduce(
+    (acc, cur) => acc + cur,
+  );
+  if (castedVoteAmount !== parsedProxyFetch.message) {
+    return {
+      message: "Failure",
+      errors: {
+        amount: `You voted ${castedVoteAmount} times, but you have ${parsedProxyFetch.message} votes. Please try again`,
+      },
+    };
   }
+
+  return {};
 }
+
+const DecisionSchema = z.record(z.string().transform((val) => Number(val)));
