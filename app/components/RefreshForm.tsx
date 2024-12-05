afda92074b6488c6e5513e6dd243569e3e77e4e4
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-12-05 14:56:54 +0100

    feat: MoonLoader on refresh form

--- app/components/RefreshForm.tsx
+++ app/components/RefreshForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { refreshForm } from "./actions";
+import { MoonLoader } from "react-spinners";
 
 export default function RefreshForm() {
   const [isDisabled, setIsDisabled] = useState(false)
@@ -18,7 +19,11 @@ export default function RefreshForm() {
       className="my-5 flex rounded-xl px-4 py-1 text-xl text-blue-950 outline outline-2 outline-blue-950 hover:bg-blue-950 hover:text-white hover:outline-none disabled:outline md:text-3xl disabled:bg-white disabled:outline-blue-950 disabled:outline-2 disabled:text-blue-950"
       disabled={isDisabled}
     >
-        Refresh Forms
+        {isDisabled ? (
+          <MoonLoader size={30} color="blue" />
+        ) : (
+          "Refresh Forms"
+        )}
     </button>
   );
 }
